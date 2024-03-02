a39a1f18ab4a2fd5e6dc0327fdf136c1d3c85a39
Ambrosio <ambrosio.rhc@gmail.com>
2024-03-02 08:54:30 -0300

    counter en vs

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import React from "react"
+
+
+import SimpleCounter from "./simplecounter";
+
+
+function App() {
+
+  return (
+		<>
+	<SimpleCounter  />,
+	
+		</>
+		
+		);
+}
+
+export default App
